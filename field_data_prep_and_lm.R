b30e635da2e741ec20c0f38359ee7c9844f18118
Danielle De La Pascua <58483225+drdelapascua@users.noreply.github.com>
2022-01-19 18:25:04 -0800

    Update field_data_prep_and_lm.R

--- field_data_prep_and_lm.R
+++ field_data_prep_and_lm.R
@@ -1,10 +1,20 @@
 ### Eda and Danielle - TM Aphid fieldwork data prep and LM
 
-# libraries
+# libraries----
+library(tidyverse)
+library(dplyr)
+library(lubridate)
 
-#set wd
+# set working directory ----
 setwd("~/GitHub/eda-aphid-project")
 
-# pull data
-data <- read.csv("TM_aphid_herbiv_data.csv")
+# pull data & clean ----
+d <- read.csv("TM_aphid_herbiv_data.csv")
+d <- d[-c(29:61), ] #drops the 'NA' rows
+d <- d[,-c(15)] #drops the last row
+
+# looking at response variables
+
+
+# looking at fixed effects
 
